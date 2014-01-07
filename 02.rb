2bcdc388c2b3f649c0c1f4b2d244e6d130d8504b
snebel <sam@samnebel.com>
2014-01-07 15:58:29 -0500

    02 solution

---
+++ 02.rb
@@ -0,0 +1,21 @@
+# By starting with 1 and 2, the first 10 terms will be:
+# 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+# By considering the terms in the Fibonacci 
+# sequence whose values do not exceed four million,
+# find the sum of the even-valued terms.
+
+# We start by noting thateven valued terms can be 
+# indexed by the 3n + 2, where n is a non-negative integer
+def get_fib(n)
+	return 1 if n == 1
+	return 2 if n == 2	
+	get_fib(n-1) + get_fib(n-2)
+end
+
+
+def sum_even_fibs_upto(limit, sum, n)
+	return sum if get_fib(3*n + 2) > limit
+	sum_even_fibs_upto(limit, sum + get_fib(3*n + 2), n+1)
+end
+
+puts sum_even_fibs_upto(4000000, 0, 0)
