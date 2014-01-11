8d4f2b3baebfd62566913eab7d0fb4e668867e96
snebel <sam@samnebel.com>
2014-01-11 15:14:15 -0500

    20 solution refactored

--- 20.rb
+++ 20.rb
@@ -1,7 +1,7 @@
 # Find the sum of the digits in the number 100!
 
-def factorial(n)
-	n == 1 ? n : n * factorial(n-1)
+def factorial(n, product = 1)
+	n == 1 ? product : factorial(n - 1, n * product)
 end
 
 def sum_digits(num_str)
