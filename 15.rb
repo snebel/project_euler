33cf325a25736357983780e162d5f401c1b12ccb
snebel <sam@samnebel.com>
2014-01-11 15:07:24 -0500

    refactored 15 solution

--- 15.rb
+++ 15.rb
@@ -1,17 +1,13 @@
 # How many such routes are there through a 20×20 grid?
-# My note: Number of paths thru n * n grid = 2n Choose n.
+# My note: Number of paths thru n * n lattice = 2n Choose n.
 
 def factorial(n, product = 1)
 	n == 1 ? product : factorial(n - 1, n * product)
 end
 
-def binom_coefficient(n, k)
+def calc_grid_paths(k)
+	n = 2 * k
 	factorial(n) / (factorial(k) * factorial(n - k))
-end
-
-# calculates number of possible paths for an n x n lattice
-def calc_grid_paths(n)
-	binom_coefficient(2*n, n)
 end
 
 puts calc_grid_paths(20)
