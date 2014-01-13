dc512e85e3125881592e3618d7af416b6451f020
snebel <nebelout@gmail.com>
2014-01-12 23:51:43 -0500

    06 solution

---
+++ 05.rb
@@ -0,0 +1,13 @@
+
+# What is the smallest positive number that is evenly
+# divisible by all of the numbers from 1 to 20?
+
+def find_product(max_div)
+	prod = max_div*(max_div - 1) # smallest possible
+	2.upto(max_div - 2) do |d|
+		
+	end
+	return prod
+end
+
+2.upto()
