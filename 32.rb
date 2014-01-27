46f29de242a7b4b566758802e359b116ce19dd5f
snebel <sam@samnebel.com>
2014-01-26 23:46:42 -0500

    refactored 32 solution

--- 32.rb
+++ 32.rb
@@ -5,33 +5,30 @@
 # identity can be written as a 1 through 9 pandigital.
 #
 # My Notes:
-# - Need total of 9 digits: 2-digit * 3-digit = 4-digit
+# - Need total of 9 digits:
+#   2-digit * 3-digit = 4-digit
+#   1-digit * 4-digit = 4-digit
 # - This is a brute force method.
 
 def is_pandigital?(m1, m2, prod)
   str = m1.to_s + m2.to_s + prod.to_s
-  arr = str.split(//)
-  arr.uniq!
-  arr.size == 9 && !arr.include?("0")
+  return false if str.size > 9
+  arr = str.split(//).uniq - ['0']
+  arr.size == 9
 end
 
-nums = []
-10.upto(99) do |multiplicand|
-  100.upto(999) do |multiplier|
-    prod = multiplicand * multiplier
-    if prod < 10000 && is_pandigital?(multiplicand, multiplier, prod)
-      nums << prod
+NUMS = []
+
+def find_pandig_products(low1, high1, low2)
+  low1.upto(high1) do |multiplicand|
+    low2.upto(10000/multiplicand) do |multiplier|
+      prod = multiplicand * multiplier
+      NUMS << prod if is_pandigital?(multiplicand, multiplier, prod)
     end
   end
 end
 
-1.upto(9) do |multiplicand|
-  1000.upto(9999) do |multiplier|
-    prod = multiplicand * multiplier
-    if prod < 10000 && is_pandigital?(multiplicand, multiplier, prod)
-      nums << prod
-    end
-  end
-end
+find_pandig_products(10, 99, 100)
+find_pandig_products(1, 9, 1000)
 
-puts nums.uniq.reduce(:+)
+puts NUMS.uniq.reduce(:+)
