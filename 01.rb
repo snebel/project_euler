2a331ffe1e14a0381410c210c653eceec43d9148
snebel <sam@samnebel.com>
2014-01-31 18:21:15 -0500

    01 solution

--- 01.rb
+++ 01.rb
@@ -1,13 +1,17 @@
 # If we list all the natural numbers below 10 that are multiples of
 # 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
+#
+# My Notes:
+# - Use sets to avoid indexing elements
+# - Set's union method functions as set-theoretic union
 
 require 'set'
 
-def find_sum_of_mults(x, y, limit)
-  set1 = Set.new((0..limit).step(3))
-  set2 = Set.new((0..limit).step(5))
+def sum_mults_upto_limit(x, y, limit)
+  set1 = Set.new((0..limit).step(x))
+  set2 = Set.new((0..limit).step(y))
   set1.union(set2).reduce(:+)
 end  
 
-puts find_sum_of_mults(3, 5, 999)
+puts sum_mults_upto_limit(3, 5, 999)
