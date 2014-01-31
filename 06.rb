7e6827670c594a3c332326cd9828ab95f3a325d0
snebel <sam@samnebel.com>
2014-01-31 18:47:43 -0500

    06 solution

--- 06.rb
+++ 06.rb
@@ -1,10 +1,14 @@
 # Find the difference between the sum of the squares of the
 # first one hundred natural numbers and the square of the sum.
+#
+# My Notes:
+# - Use standard formulas to find sum of first n squares and
+#   sum of the first n natural numersg
 
 def find_difference(n)
-	n = n.to_f
-	sqs_sum = (n * (2 * n + 1) * (n + 1)) / 6
-	(n * (n + 1) / 2)**2 - sqs_sum
+  sq_sum = (n.to_f * (n + 1) / 2)**2
+  sum_sqs = (n * (2 * n + 1) * (n + 1)) / 6
+  sq_sum - sum_sqs
 end
 
 puts find_difference(100)
