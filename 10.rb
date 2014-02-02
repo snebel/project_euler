4a5c3bbe5cf10a36c9d38c174d21c437ad691dc3
snebel <sam@samnebel.com>
2014-02-01 23:38:37 -0500

    prime generator demonstration

--- 10.rb
+++ 10.rb
@@ -1,16 +1,53 @@
 # Find the sum of all the primes below two million.
+#
+# My Notes:
+# - Here are three ways to sum all primes below an arbitrary limit.
+# - The first uses the Sieve of Eratosthenes, which is by far the fastest
+#   way to generate prime numbers.
+# - The second uses the more intuitive method of checking each number upto
+#   the limit for primality, with the slight optimization of checking only
+#   odd numbers above 2.
+# - The third uses Ruby's Prime class prime number generator, which according
+#   to the documentation uses Eratosthenes' method, but is still about five
+#   times slower than the sieve I've written.
+
+########Sieve of Eratosthenes#########
+beginning = Time.now
+
+def find_primes_upto(n)
+  list = (2..n).to_a
+  2.upto(n/2) do |p|
+    (2*p-2..n-2).step(p){ |i| list[i] = 0 } unless list[p-2] == 0
+  end
+  list
+end
+
+puts find_primes_upto(2_000_000).reduce(:+)
+
+puts "Sieve time: #{Time.now - beginning} seconds"
+puts
+
+########Checking Primes Manually##########
+beginning = Time.now
 
 def is_prime?(n)
-  2.upto(Math.sqrt(n).to_i) { |num| return false if n % num == 0 }
+  2.upto(Math.sqrt(n).to_i) { |d| return false if n % d == 0 }
   n == 1 ? false : true
 end
 
-def sum_primes_upto(limit)
-	sum = 0
-	2.upto(limit) { |n| sum += n if is_prime? n }
-	sum
-end
+lim = 2_000_000
+puts (3..lim).step(2).map{ |p| is_prime?(p) ? p : 0 }.reduce(:+)+2
 
-puts sum_primes_upto(2000000)
+puts "Manual prime time: #{Time.now - beginning} seconds"
+puts
 
-# NOTE: sieve of Eratosthenes would be more efficient, but require more code.
+########Ruby's Prime Number Generator#########
+beginning = Time.now
+require 'prime'
+
+lim = 2_000_000
+puts Prime.each(lim).map{|p| p}.reduce(:+)
+
+puts "Ruby Prime time: #{Time.now - beginning} seconds"
+puts
+
