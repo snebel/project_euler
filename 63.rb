3b6d3f83a156ab16bbbc797f1d56b043c81186c5
snebel <sam@samnebel.com>
2014-02-15 18:53:41 -0500

    63 solution

--- 63.rb
+++ 63.rb
@@ -6,22 +6,22 @@
 # My Notes:
 # - Given an n, we need to to check j^n for all natural numbers
 #   j until j^n is larger than the largest n-digit number
-# - We can initially let j = log(base n)(10^(n-1)) fore each n
+
 
 def count_n_digit_powers(n)
   min = 10**(n-1)
-  j = (Math.log(min, n)+1).to_i
-  power = j**n
+  j = 1
+  power = 1
   count = 0
-  while  power <= 10**n
-    count += 1
+  while  power < 10**n
+    count += 1 unless power < min
+    j += 1 
     power = j**n
-    j += 1    
   end 
   count
 end
 
-def sum_num_n_digit_powers(n = 2, sum = 0)
+def sum_num_n_digit_powers(n = 1, sum = 0)
   c = count_n_digit_powers(n)
   c == 0 ? sum : sum_num_n_digit_powers(n + 1, sum + c)
 end
