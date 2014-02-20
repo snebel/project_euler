7d52f38efc167ee05379d62c0ffee1d82cbb42ff
snebel <sam@samnebel.com>
2014-02-19 21:11:52 -0500

    81 solution

--- 81.rb
+++ 81.rb
@@ -12,43 +12,49 @@
 # matrix, from the top left to the bottom right by only moving right and down.
 #
 # My Notes:
-# - Lets start by using a similar algorithm to problem 67:
+# - We can use a similar algorithm to problem 67, treating the matrix as a tree whos root
+#   is the top-left element and whose rows are the diagonals.
 #   Begin with the top-left element, look at the two elements reachable from this point,
 #   select the lesser of the two, set its value to itself + the previous element. Repeat
 #   this process until the lower-right corner is reached. 
+# - For an n x n matrix, there are 2*n - 1 diagonals.
+# - To grab the jth diagonal upto the nth diagonal, we can start with the first
+#   element of the jth row, and work our way up until row 0.
+# - To grab the remaining diagonals, we can start at any element of the last row
+#   and work our way up until we reach an element of the last column.
 
-test = [
-  [131,673,234,103,18],
-  [201,96,342,965,150],
-  [630,803,746,422,111],
-  [537,699,497,121,956],
-  [805,732,524,37, 331],
-]
+def change_node(matrix, j, k) # row, column index
+  if j == 0
+    matrix[j][k] += matrix[j][k-1]
+  elsif k == 0
+    matrix[j][k] += matrix[j-1][k]
+  else
+    matrix[j][k] += [matrix[j-1][k], matrix[j][k-1]].min
+  end
+end
+
+def change_top_diag(matrix, i)
+  (i-1).downto(0) do |j|
+    change_node(matrix, j, i - j - 1)
+  end
+end
+
+def change_bottom_diag(matrix, i)
+  last_row = matrix.size - 1
+  (last_row).downto(i - last_row - 1) do |j|
+    change_node(matrix, j, i - j - 1)
+  end 
+end
+
+def find_shortest_path(matrix)
+  n = matrix.size
+  (2..n).each {|d| change_top_diag(matrix, d)}
+  (n+1..2*n-1).each {|d| change_bottom_diag(matrix, d)}
+  matrix[n-1][-1]
+end
 
 matrix = File.readlines('./files/matrix.txt').map do |line| 
   line.split(',').map(&:to_i)
 end
 
-def check_children(matrix, j=0, k=0) # row, column
-  current = matrix[j][k]
-  p current
-  return current if j == 4 && k == 4
-  if j == 4
-    matrix[j][k+1] += current
-    check_children(matrix, j, k+1)
-  elsif k == 4
-    matrix[j+1][k] += current
-    check_children(matrix, j+1, k)
-  else
-    matrix[j+1][k] += current 
-    matrix[j][k+1] += current
-    down, right = matrix[j+1][k], matrix[j][k+1]
-    down <= right ? j += 1 : k += 1
-    
-    check_children(matrix, j, k)
-  end
-end
-
-p check_children(test)
-
-
+p find_shortest_path(matrix)
