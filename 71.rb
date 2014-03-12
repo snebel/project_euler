f7a22a5eabc6e3cdeffd5159a7483bbdfb17eecc
snebel <sam@samnebel.com>
2014-03-12 01:49:57 -0400

    71 solution

--- 71.rb
+++ 71.rb
@@ -2,19 +2,16 @@
 # of size, find the numerator of the fraction immediately to the left of 3/7.
 #
 # My Notes:
-# - Ruby's Rational class will be helpful
-# - Only need to list proper fractions upto 3/7 with denominator ≤ 1,000,000
-# - Or can try to find the closest possible fraction below 3/7 with a d ≤ 1,000,000
-# - get all pairs of numbers that are relatively prime and form a rational below 3/7?
+# - We can use Ruby's Rational class to handle reducing fractions 
+# - For each divisor, d, we can find the rational closest to but less than 3/7 by
+#   setting the numerator to d*3 / 7 with integer division.
+# - We can list these rationals for all possible divisors and grab the largest one
 
-def gcd(a, b)
-  b == 0 ? a : gcd(b, a % b)
+# for each divisor, find the fraction closest to 3/7
+
+def closest_fraction_to(num, denom, max_divisor)
+  fracs = (2..max_divisor).map {|d| Rational(d*num/denom, d)}
+  (fracs - [Rational(num, denom)]).max
 end
 
-limit = Rational(3, 7)
-
-def average(frac, limit)
-  (frac + limit) / 2 
-end
-
-2.upto(1_000_000).each do |
+p closest_fraction_to(3, 7, 1_000_000)
