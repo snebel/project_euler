956b7db497d918a2304da733250545e401941265
snebel <sam@samnebel.com>
2014-04-26 19:13:34 -0400

    05 solution

--- 05.rb
+++ 05.rb
@@ -1,19 +1,21 @@
-# What is the smallest positive number that is evenly
-# divisible by all of the numbers from 1 to 20?
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+#
+# My Notes:
+# - We need to find the least common multiple of multiple numbers.
+# - We can do this by listing the numbers, finding the lcm, l, of the first two numbers,
+#   then setting l to the lcm of l and the next number in the list, and repeating the
+#   process until the end of the list is reached.
+# - To find the lcm of 1,...,n we can just find the lcm of n/2 + 1,...,n since every
+#   number in the former list must be a factor of a number in the latter list.
 
-# Euclid's algorithm
+# Euclid's algorithm for gcd
 def gcd(a, b)
-	b == 0 ? a : gcd(b, a % b)
+  b == 0 ? a : gcd(b, a % b)
 end
 
 def lcm(a, b)
-	(a * b) / gcd(a, b)
+  (a * b) / gcd(a, b)
 end
 
-def find_lcm_upto(max)
-  product = 2
-  3.upto(max) {|n| product = lcm(n, product)}
-  product
-end
-
-puts find_lcm_upto(20)
+p (11..20).reduce(&:lcm)
