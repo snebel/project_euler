5fc34f5dc634a9a06d7bc851d1356d622ad8423e
snebel <sam@samnebel.com>
2014-04-26 19:28:13 -0400

    04 solution

--- 04.rb
+++ 04.rb
@@ -1,27 +1,18 @@
-# A palindromic number reads the same both ways.
-# The largest palindrome made from the product
+# A palindromic number reads the same both ways. The largest palindrome made from the product
 # of two 2-digit numbers is 9009 = 91 × 99.
-# Find the largest palindrome made from the 
-# product of two 3-digit numbers.
-
-def is_palindrome?(n)
-	s = n.to_s
-	s == s.reverse
-end
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
 
 def max_palindrome_product(limit)
 	products = []
 	1.upto(limit) do |j|
 		1.upto(limit) do |k|
-			products << j*k
+			prod = j * k
+			products << prod if prod.to_s == prod.to_s.reverse
 		end
 	end
-	max_palindrome(products)
+	products.max
 end
 
-def max_palindrome(nums)
-	nums.select{|num| is_palindrome?(num)}.max
-end
-
-puts max_palindrome_product(999)
+p max_palindrome_product(999)
 
