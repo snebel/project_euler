6ae868fe62658c53f45a7d45f1236501235d9321
snebel <sam@samnebel.com>
2014-04-28 13:58:19 -0400

    25 solution

--- 25.rb
+++ 25.rb
@@ -1,13 +1,9 @@
-# What is the first term in the Fibonacci
-# sequence to contain 1000 digits?
+# What is the first term in the Fibonacci sequence to contain 1000 digits?
 
-def fib(n, prev = 1, sum = 1)
-  n <= 2 ? sum : fib(n - 1, sum, prev + sum)
+def first_fib_above(lim)
+  fibs = [1, 1]
+  fibs << fibs[-1] + fibs[-2] while fibs[-1] < lim 
+  fibs.size
 end
 
-def first_n_digit_fib(n)
-  limit = 10**(n-1)
-  1.upto(Float::INFINITY) { |i| return i if fib(i) >= limit }
-end
-
-puts first_n_digit_fib(1000)
+p first_fib_above(10**999)
